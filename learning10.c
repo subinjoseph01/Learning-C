a5782f1441b2b1d9051eb1c1c68cf3e084e89e19
subin3439 <josephsubinn@gmail.com>
2025-07-01 16:47:54 +0530

    lora

---
+++ learning10.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+    int num = 1;
+
+    while (num <=20)
+
+    {
+        if (num%2==0)
+        {
+            printf("%d \n", num);
+        }
+        
+        num++;
+        
+    }
+    return 0;
+}
+
+// how to print even numbers using while loop up to 20 
