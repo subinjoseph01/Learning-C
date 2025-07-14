345e075c2bce096aced8ae157412f068d62fcbad
subin3439 <josephsubinn@gmail.com>
2025-07-14 17:19:37 +0530

    2darraysizeof

--- Learning17.c
+++ Learning17.c
@@ -4,17 +4,43 @@ int main()
 
 {
 
-    int numbers[2][3] =
-        {{88, 55, 66},
-         {22, 99, 11}};
+int numbers[4][3]= 
+{{12,23,21},
+{31,76,11,},
+{66,33,87},
+{23,65,87}};
 
-    for (int r = 0; r <= 1; r++)
+int row = sizeof(numbers)/ sizeof(numbers[0]);
+int col= sizeof(numbers[0])/sizeof(numbers[0][0]);
+
+// The given size of numbers indicates every single bytes 
+// which means 1 int is = 4 bytes 
+
+for (int r=0; r<row; r++)
+{
+    for(int j=0; j<col; j++)
     {
-        for (int k = 0; k <= 2; k++)
-        {
-            printf("%d", numbers[r][k]);
-           printf("\n");
-        }
+        printf("%d",numbers[r][j]);
+        printf("\n");
+        
     }
-    return 0;
 }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Here we did 2d array using for loop and printed  using size of bytes suucessfully
